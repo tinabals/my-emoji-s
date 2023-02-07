964833d0f1d68fa41081069cf1c0627926c7bcc8
Damilola Balogun <balogundamilola.f@gmail.com>
2023-02-07 22:28:25 +0100

    implemented the functionalities
     I  also changed a couple of things and added some new functions too. Emojis can be added at the start or end and delete both ways too. It is being stored in the local storage.

--- index.js
+++ index.js
@@ -1,41 +1,63 @@
+
+
 const inputEmojiField = document.getElementById("input-emoji");
 const pushBtn = document.getElementById("push-btn");
 const shiftBtn = document.getElementById("shift-btn");
 const popBtn = document.getElementById("pop-btn");
 const unshiftBtn = document.getElementById("unshift-btn");
+const clearBtn = document.getElementById("clear-btn");
+const emojiRegex = (/(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g)
 let emojiDisplayContainer = document.getElementById("emoji-container");
-let savedEmojis = [];
+let savedEmojis = Array();
 
-renderEmojis = () => {
-  emojiDisplayContainer.innerHTML = "";
+
+const renderEmojis = () => {
+  emojiDisplayContainer.innerHTML = ""
   let emojis = "";
-  let retrievedData = localStorage.getItem("myEmoji");
-  savedEmojis = JSON.parse(retrievedData);
+  let retrievedData = localStorage.getItem("myEmoji")
+  savedEmojis = JSON.parse(retrievedData) || [];
   if (savedEmojis) {
     for (let i = 0; i < savedEmojis.length; i++) {
-      emojis += `<span> ${savedEmojis[i]} </span>`;
+      emojis += `<span> ${savedEmojis[i]} </span>`
     }
   }
-  emojiDisplayContainer.innerHTML = emojis + " ";
-};
+  emojiDisplayContainer.innerHTML = emojis + " "
+ } 
+ 
 
-renderEmojis();
 
-addEmoji = (emoji) => {
-  if (emoji) {
-    if (!savedEmojis) savedEmojis = [];
-    savedEmojis.push(emoji);
-    console.log(savedEmojis);
+const addEmoji = (emoji, position) => {
+       if (!emoji){
+        return alert("Type in an Emoji pls")
+       }
+       if (!emoji.match(emojiRegex)){
+        inputEmojiField.value = ""
+        return alert("Only emojis are required!")
+       }
+        position === 'start' ? savedEmojis.push(emoji) : savedEmojis.unshift(emoji)
+        localStorage.setItem("myEmoji", JSON.stringify(savedEmojis))
+        renderEmojis();
+        inputEmojiField.value = ""
+}
+
+
+const deleteEmoji = (type) => {
+    type === 'pop' ? savedEmojis.pop() : savedEmojis.shift()
     localStorage.setItem("myEmoji", JSON.stringify(savedEmojis));
-    renderEmojis();
-    inputEmojiField.value = "";
-  }
-};
-
-deleteEmojiFromEnd = () => {
-    alert("hi")
     renderEmojis()
 }
 
-pushBtn.addEventListener("click", () => addEmoji(inputEmojiField.value));
-popBtn.addEventListener('click', deleteEmojiFromEnd)
+const DeleteAllEmojis = () => {
+  localStorage.clear()
+  renderEmojis()
+}
+
+
+
+pushBtn.addEventListener("click", () => addEmoji(inputEmojiField.value, 'start'));
+unshiftBtn.addEventListener('click', () => addEmoji(inputEmojiField.value ) )
+popBtn.addEventListener('click',() => deleteEmoji('pop'))
+shiftBtn.addEventListener('click', deleteEmoji)
+clearBtn.addEventListener('click', DeleteAllEmojis)
+
+renderEmojis()
